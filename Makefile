6228ecc9c234256f0e4e1626db836285b426d8d9
Raj Sahae <rsahae@teslamotors.com>
2015-06-01 16:18:09 -0700

    Moved game logic into its own library

--- Makefile
+++ Makefile
@@ -6,9 +6,9 @@ CFLAGS=-Wall -g -I$(SRC)
 EXAMPLES=ex1 ex2 ex3 ex4 ex5      \
 	 ex6 ex7 ex8 ex9 ex10     \
 	 ex11 ex12 ex13 ex14 ex15 \
-	 ex16 ex17 ex18 ex19
+	 ex16 ex17 ex18
 
-all: $(EXAMPLES)
+all: $(EXAMPLES) ex19
 
 folders: 
 	mkdir -p bin
@@ -23,11 +23,14 @@ clean:
 $(EXAMPLES): folders
 	$(CC) $(CFLAGS) -o $(BIN)/$@ $(SRC)/$@.c
 
-ex19: object.o
-	$(CC) $(CFLAGS) -o $(BIN)/$@ $(OBJ)/object.o $(SRC)/$@.c
+ex19: object game
+	$(CC) $(CFLAGS) -o $(BIN)/$@ $(OBJ)/object.o $(OBJ)/game.o $(SRC)/$@.c
 
-object.o: folders
+object: folders
 	$(CC) $(CFLAGS) -c -o $(OBJ)/object.o $(SRC)/object.c
+
+game: folders
+	$(CC) $(CFLAGS) -c -o $(OBJ)/game.o $(SRC)/game.c
 
 test: ex17 ex19
 	test/ex17_test.sh
--- src/ex19.c
+++ src/ex19.c
@@ -1,103 +1,13 @@
 #include <stdio.h>
-#include <errno.h>
 #include <stdlib.h>
-#include <string.h>
 #include <time.h>
 #include <assert.h>
 
-#include "ex19.h"
-
-int Monster_attack(void* self, int damage)
-{
-  assert(self);
-  assert(damage >= 0);
-
-  Monster *monster = self;
-  assert(monster->hit_points > 0);
-
-  if (damage == 0) {
-    printf("WHOOSH! Your attack missed!\n");
-  } else {
-    printf("You attack %s for %d damage!\n", monster->_(description), damage);
-    monster->hit_points -= damage;
-  }
-
-  if (monster->hit_points > 0) {
-    printf("%s still has %d hitpoints!\n", monster->_(description), monster->hit_points);
-    return 0;
-  } else {
-    printf("It is dead!\n");
-    return 1;
-  }
-}
+#include "game.h"
 
 Object MonsterProto = {
   .attack = Monster_attack
 };
-
-
-void* Room_move(void* self, Direction direction)
-{
-  assert(self);
-
-  Room* room = self;
-  Room* next = NULL;
-
-  if (direction == NORTH && room->north) {
-    printf("You go north, into:\n");
-    next = room->north;
-  } else if (direction == SOUTH && room->south) {
-    printf("You go south, into:\n");
-    next = room->south;
-  } else if (direction == EAST && room->east) {
-    printf("You go east, into:\n");
-    next = room->east;
-  }  else if (direction == WEST && room->west) {
-    printf("You go west, into:\n");
-    next = room->west;
-  } else {
-    printf("You can't go that direction");
-    next = NULL;
-  }
-
-  if (next)
-    next->_(describe)(next);
-
-  return next;
-}
-
-int Room_attack(void* self, int damage)
-{
-  assert(self);
-  assert(damage >= 0);
-
-  Room* room = self;
-  Monster* monster = room->bad_guy;
-
-  if (monster) {
-    if (monster->_(attack)(monster, damage)) {
-      free(room->bad_guy);
-      room->bad_guy = NULL;
-    }
-    return 1;
-  } else {
-    printf("You flail in the air at nothing...Idiot\n");
-    return 0;
-  }
-}
-
-void Room_describe(void* self)
-{
-  assert(self);
-
-  Room* room = self;
-
-  if (room->proto.description)
-    printf("%s\n", room->proto.description);
-
-  if (room->bad_guy)
-    printf("You see %s", room->bad_guy->_(description));
-}
 
 Object RoomProto = {
   .move = Room_move,
@@ -105,37 +15,11 @@ Object RoomProto = {
   .describe = Room_describe
 };
 
-
-void* Map_move(void* self, Direction direction)
-{
-  assert(self);
-
-  Map* map = self;
-  Room* location = map->location;
-  Room* next = NULL;
-
-  assert(location);
-
-  next = location->_(move)(location, direction);
-
-  if (next)
-    map->location = next;
-
-  return next;
-}
-
-int Map_attack(void* self, int damage)
-{
-  assert(self);
-  assert(damage >= 0);
-
-  Map* map = self;
-  Room* location = map->location;
-
-  assert(location);
-
-  return location->_(attack)(location, damage);
-}
+Object MapProto = {
+  .init = Map_init,
+  .move = Map_move,
+  .attack = Map_attack
+};
 
 int Map_init(void* self)
 {
@@ -200,63 +84,6 @@ int Map_init(void* self)
   return 1;
 }
 
-Object MapProto = {
-  .init = Map_init,
-  .move = Map_move,
-  .attack = Map_attack
-};
-
-int process_input(Map* game)
-{
-  assert(game);
-
-  printf("\n> ");
-
-  char ch = getchar();
-  getchar(); // eat ENTER
-
-  int damage = rand() % 4;
-
-  switch(ch) {
-    case EOF:
-      printf("Giving up? You suck\n");
-      return 0;
-
-    case 'n':
-      game->_(move)(game, NORTH);
-      break;
-
-    case 's':
-      game->_(move)(game, SOUTH);
-      break;
-
-    case 'e':
-      game->_(move)(game, EAST);
-      break;
-
-    case 'w':
-      game->_(move)(game, WEST);
-      break;
-
-    case 'a':
-      game->_(attack)(game, damage);
-      break;
-
-    case 'l':
-      printf("You can go:\n");
-      if (game->location->north) printf("North\n");
-      if (game->location->south) printf("South\n");
-      if (game->location->east ) printf("East\n" );
-      if (game->location->west ) printf("West\n" );
-      break;
-
-    default:
-      printf("What?: %c\n", ch);
-  }
-
-  return 1;
-}
-
 int main(int argc, char* argv[])
 {
   // seed random number generator
@@ -271,7 +98,7 @@ int main(int argc, char* argv[])
   printf("You enter the ");
   game->location->_(describe)(game->location);
 
-  while(process_input(game)) {}
+  while(run(game)) {}
 
   return 0;
 }
